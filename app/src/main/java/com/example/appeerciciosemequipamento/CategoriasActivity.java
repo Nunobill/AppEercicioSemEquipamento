f97c05122d14c8fcffb4f0b4a5c843527b072c0f
Nuno Bill Afonso da Silva <1012377@sal.ipg.pt>
2019-03-22 15:20:53 +0000

    cração do layout1.1

--- app/src/main/java/com/example/appeerciciosemequipamento/CategoriasActivity.java
+++ app/src/main/java/com/example/appeerciciosemequipamento/CategoriasActivity.java
@@ -1,32 +1,22 @@
 package com.example.appeerciciosemequipamento;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class CategoriasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_categorias);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-
-
     }
 
-    public void Começar(View view) {
-        Toast.makeText(this, "Vamos começar...", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, CategoriasActivity.class);
-        startActivity(intent);
-    }
 }
